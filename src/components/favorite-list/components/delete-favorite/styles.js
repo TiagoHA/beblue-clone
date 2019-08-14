1f1326d8e6ac4606910f1900ca2dc05408417ee1
Tiago Almeida <Tiago.Almeida@clickqi.com.br>
2019-08-14 20:58:16 -0300

    feat(delete-favorite): add component

---
+++ src/components/favorite-list/components/delete-favorite/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonDelete = styled.button`
+  background-color: transparent;
+  padding: 5px 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 60px;
+
+  img {
+    margin-right: 10px;
+  }
+
+  p {
+    color: #bdc4c9;
+    padding-top: 2px;
+    font-weight: 600;
+    font-size: 15px;
+  }
+`;
---
+++ src/components/favorite-list/components/tag/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const TagContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 5px;
+`;
+
+export const ButtonDelete = styled.div`
+  background-color: #1f68c1;
+  padding: 3px 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 60px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+
+  p {
+    display: flex;
+    color: #fff;
+    font-size: 10px;
+    text-align: center;
+    align-items: center;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    padding: 2px;
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-left: 7px;
+      cursor: pointer;
+    }
+  }
+`;
