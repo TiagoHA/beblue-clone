29972ab4a244bdaebdf81e5fd0166476e0df342f
Tiago Almeida <Tiago.Almeida@clickqi.com.br>
2019-08-28 14:09:58 -0300

    feat(global styles): rename file

---
+++ src/styles/colors.js
@@ -0,0 +1,26 @@
+const Colors = {
+  bg: {
+    primary: '',
+    secondary: '',
+    success: '',
+    danger: '',
+    warning: '',
+    info: '',
+    light: '',
+    dark: '',
+    white: '',
+  },
+  text: {
+    primary: '',
+    secondary: '',
+    success: '',
+    danger: '',
+    warning: '',
+    info: '',
+    light: '',
+    dark: '',
+    white: '',
+  }
+};
+
+export { Colors };
